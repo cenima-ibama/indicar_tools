00c09c450366738de3f4f29b0c823feaea9f547b
Wille Marcel <wille@wille.blog.br>
2014-10-14 13:50:36 -0300

    update DESCRIPTION

--- indicar/indicar.py
+++ indicar/indicar.py
@@ -23,15 +23,15 @@ to process Landsat 8 imagery.
         Process: creates RGB, NDVI and a polygon containing the losses in the
         vegetation of the imagery in comparison with the NDVI of the same
         scene generated 16 days ago.
-        indicar.py process path
+        indicar process path
 
         RGB: creates only a RGB image, using the bands 6, 5 and 4. This RGB
         gives emphasys on the areas without vegetation.
-        indicar.py process --rgb path
+        indicar process --rgb path
 
         NDVI: creates only a NDVI image. Where there are clouds or cirrus,
         the pixel value will be 0.
-        indicar.py process --ndvi path
+        indicar process --ndvi path
 """
 
 
